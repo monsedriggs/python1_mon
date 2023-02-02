51b050ad056474520ccd28ff98a22d34ec6e8bff
Carlos Mario Arguedas Barahona <39394819+cargbar@users.noreply.github.com>
2023-02-01 19:28:05 -0600

    Funcion de suma

---
+++ tarea_3/operaciones_fndamentales.py
@@ -0,0 +1,8 @@
+# Definir la funcion 
+def calcular_suma(operacion_math):
+   #Variable resultado
+    result = eval(operacion_math)
+# definir variable Operacion math
+    print(result )
+operacion_math = input("ingrese la suma que desea realizar: " )
+calcular_suma (operacion_math)
