ad07e112c096e4fdfa370cdb7827852629b7114f
Monserrath Aguilar Driggs <monserrath.aguilar@ucr.ac.cr>
2023-02-01 19:02:48 -0600

    funciones aritmeticas, correr y archivo txt

---
+++ tarea_3/aritméticas2.py
@@ -0,0 +1,16 @@
+#calcula la división  entera de dos numeros
+def calcular_division(num_1, num_2):
+    
+    pass
+
+#calcula el factorial de un numero
+def calcular_factorial(n):
+    if n==0:
+        return 1
+    else:
+        return n *calcular_factorial(n-1)
+
+#calcula la potencia de un numero
+def calcular_potencia(base, exponente):
+
+    pass
--- tarea_3/calculadora.py
+++ tarea_3/calculadora.py
@@ -1,2 +1,50 @@
+import aritméticas2
+
 def correr():
-    pass
+   while True:
+        opcion= input("\t+) Suma\n\t-) Resta\n\t*) Multiplicación\n\t//) División\n\t!) Factorial\n\t^) Potencia\n\t~) Salir\nIngrese una opción: ")
+        #ejecuta la suma
+        if opcion== "+":
+            #pide los numeros al usuario
+            
+            print(f"El resultado de la suma es: ")#imprime el resultado
+            pass
+        #ejecuta la resta
+        elif opcion== "-":
+            #pide los numeros al usuario
+            num_minuendo= int(input("Digite el minuendo de la resta: "))
+            num_sustraendo= int(input("Digite el sustraendo de la resta: "))
+            print(f"El resultado de la resta es: ")#imprime el resultado
+            pass
+        #ejecuta la multiplicación
+        elif opcion== "*":
+            #pide los numeros al usuario
+            print(f"El resultado de la multiplicación es: ")#imprime el resultado
+            pass
+        #ejecuta la división
+        elif opcion== "//":
+            #pide los numeros al usuario
+            num_dividendo= int(input("Digite el dividendo de la división: "))
+            num_divisor= int(input("Digite el divisor de la divsión: "))
+            print(f"El resultado de la división es: {aritméticas2.calcular_division(num_dividendo, num_divisor)}") #imprime el resultado
+        #ejecuta el factorial
+        elif opcion== "!":
+            #pide el numero al usuario
+            num_factorial= int(input("Calcular el factorial de: "))
+            if num_factorial<0:
+                print("Error, el numero debe ser entero positivo")
+            else:
+                print(f"El factorial es: {aritméticas2.calcular_factorial(num_factorial)}")#imprime el resultado
+        #ejecuta la potencia
+        elif opcion== "^":
+            #pide los numeros al usuario
+            num_base= int(input("Digite la base de la potencia: "))
+            num_exponente= int(input("Digite el exponente de la potencia: "))
+            print(f"La potencia es: {aritméticas2.calcular_potencia(num_base, num_exponente)}")#imprime el resultado
+        #salir
+        elif opcion== "~":
+            break
+        else:
+            print("Error, opción invalida, intente de nuevo")
+        print()
+correr()
