125eee04956837b540695d2483eab57c36b2e409
monsedriggs <122844882+monsedriggs@users.noreply.github.com>
2023-02-04 17:07:04 -0600

    Delete tarea_3 directory

--- tarea_3/operaciones_fndamentales.py
+++
@@ -1,8 +0,0 @@
-# Definir la funcion 
-def calcular_suma(operacion_math):
-   #Variable resultado
-    result = eval(operacion_math)
-# definir variable Operacion math
-    print(result )
-operacion_math = input("ingrese la suma que desea realizar: " )
-calcular_suma (operacion_math)
