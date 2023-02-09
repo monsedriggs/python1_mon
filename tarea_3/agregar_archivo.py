4e98c76170da919f4ecdf467de782cd6e4886b26
Monserrath Aguilar Driggs <monserrath.aguilar@ucr.ac.cr>
2023-02-08 21:25:51 -0600

    tarea3 terminada

--- tarea_3/agregar_archivo.py
+++ tarea_3/agregar_archivo.py
@@ -2,17 +2,25 @@
 def agregar_operaciones(operacion):
     #abre el archivo
     archivo= open("resultados.txt", "a")
-    archivo.write(operacion)
+    archivo.write(f"La operacion realizada fue: {operacion}")
     archivo.write(",")
     #cerrar el archivo
     archivo.close()
 
+#agrega los numeros 
+def agregar_numeros(numeros):
+    #abre el archivo
+    archivo= open("resultados.txt", "a")
+    archivo.write(f" Los numeros utilizados en la operacion son: {numeros}")
+    archivo.write(",")
+    #cierra el archivo
+    archivo.close()
 
 #agrega el resultado
 def agregar_resultados(resultado):
     #abre el archivo
     archivo= open("resultados.txt", "a")
-    archivo.write(resultado)
+    archivo.write(f" El resultado es: {resultado}")
     archivo.write(f"\n")
     #cerrar el archivo
     archivo.close()
